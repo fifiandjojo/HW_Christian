0fdf1b2996f43b76491a5c9c3542aa0a07459321
Caleb Chris <chairmancaleb@gmail.com>
2025-02-12 20:52:31 -0500

    Print the mean and SD

--- Christian_hw.R
+++ Christian_hw.R
@@ -1,3 +1,4 @@
 random_numbers <- rnorm(100, mean = 25, sd = 1)
 mean_value <- mean(random_numbers)
 sd_value <- sd(random_numbers)
+print(paste("The mean is", mean_value, "and the Standard Deviation is", sd_value))
